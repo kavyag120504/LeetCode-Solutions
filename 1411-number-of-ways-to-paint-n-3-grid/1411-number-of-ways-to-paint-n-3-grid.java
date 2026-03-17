c2860643d7ce9bfba44eb5f7fecd01653d0cdf35
Kavya Goswami <kavya.goswami.23cse@bmu.edu.in>
2026-03-17 20:23:19 +0530

    Time: 3 ms (96.51%), Space: 42.1 MB (76.27%) - LeetHub

---
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numOfWays(int n) {
+        long MOD=1000000007;
+        long a=6;
+        long b=6;
+        for(int i=2;i<=n;i++){
+           long newA=(3*a+2*b) % MOD;
+           long newB=(2*a+2*b) % MOD;
+           a=newA;
+           b=newB;
+        }
+        return (int)((a+b)% MOD);
+    }
+}
