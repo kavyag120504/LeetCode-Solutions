d1bc44ecb26bb7b432c4878d494ce9ae7c6a95b7
Kavya Goswami <kavya.goswami.23cse@bmu.edu.in>
2026-03-17 20:16:07 +0530

    Time: 0 ms (100%), Space: 43 MB (46.86%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxDepth(String s) {
+        int c=0;int max=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('){
+                c++;
+                max=Math.max(max,c);
+            }
+            else if(ch==')'){
+                c--;
+            }
+        }
+        return max;
+    }
+}
