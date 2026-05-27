2bcb1d984cddc496dbb1b17cacb785bf4663807d
Kavya Goswami <goswamikavya47@gmail.com>
2026-05-27 15:53:02 +0530

    Time: 3 ms (87.02%), Space: 71.8 MB (59.93%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -1,21 +1,27 @@
 class Solution {
     public int numOfSubarrays(int[] arr, int k, int threshold) {
-        int Countavg=0;
-        int sum=0;
-        int totalSum=0;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-          sum+=arr[i];
-          
-          if(i>=k)
-          sum-=arr[i-k];
-        
-        if(i>=k-1){
-           int avg= sum/k;
-           if(avg>=threshold)
-           Countavg++;
+
+        int requiredSum = k * threshold;
+
+        int windowSum = 0;
+        int count = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+
+            // add current element
+            windowSum += arr[i];
+
+            // remove element if window size exceeds k
+            if(i >= k) {
+                windowSum -= arr[i-k];
+            }
+
+            // check when window size becomes k
+            if(i >= k-1 && windowSum >= requiredSum) {
+                count++;
+            }
         }
-        }
-        return Countavg;
+
+        return count;
     }
 }
