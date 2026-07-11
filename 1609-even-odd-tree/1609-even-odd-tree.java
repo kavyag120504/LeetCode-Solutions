782716d6d7796bd1c56ad523d4ff4565da1a6072
Kavya Goswami <goswamikavya47@gmail.com>
2026-07-11 18:19:07 +0530

    Time: 10 ms (85.51%), Space: 78.1 MB (27.55%) - LeetHub

---
+++ 1609-even-odd-tree/1609-even-odd-tree.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isEvenOddTree(TreeNode root) {
+      if (root == null) return true;
+      Queue<TreeNode> queue=new LinkedList<>();
+      queue.offer(root);
+      int level=0;
+
+      while(!queue.isEmpty()){
+        int levelSize=queue.size();
+           int prev = (level % 2 == 0) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+          
+          for(int i=0;i<levelSize;i++){
+
+              TreeNode node = queue.poll();
+              int val=node.val;
+
+            if(level%2==0){
+              if(val % 2 ==0 || node.val<=prev)
+              return false;
+            }
+
+              else{
+            if (val % 2 != 0 || val >= prev) return false;
+              }
+
+              prev =node.val;
+              if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);   
+
+          }
+          
+          level++;
+
+         }
+      return true;
+    }
+}
